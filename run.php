dd7933e4faaf079541fba432378735f44979bd57
Kev Provance <kevin.provance@gmail.com>
2023-02-19 01:20:40 -0500

    Get Redux item caches.

--- run.php
+++ run.php
@@ -12,11 +12,20 @@ require_once dirname( __FILE__ ) . '/vendor/autoload.php';
 $key    = $argv[1];
 $token  = new Herbert\Envato\Auth\Token( $key );
 $client = new Herbert\EnvatoClient($token);
-
-$list = $client->catalog->collection(['id' => 4201392 ,'page' => 1]);
+$list   = $client->catalog->collection(['id' => 4201392 ,'page' => 1]);
+$count  = $list->results['pagination']['pages'];
 
 echo "Saving Page Count File\n\n";
 file_put_contents( $count_file, $list->results['pagination']['pages'] );
+
+echo "Saving Redux Item Caches\n\n";
+for ( $i = 1; $i <= $count; $i ++ ) {
+	$data  = $client->catalog->collection(['id' => 4201392 ,'page' => $i]);
+	$json  = json_encode( $data->results );
+	$cache = dirname( __FILE__ ) . 'envato-market-' . $i . '.json';
+
+	file_put_contents( $cache, $json );
+}
 
 return;
 
