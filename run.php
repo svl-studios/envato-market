c041a255de09e3558714eb12c121d04c872fad11
Kev Provance <kevin.provance@gmail.com>
2023-02-19 00:25:26 -0500

    Envato API

--- run.php
+++ run.php
@@ -1,98 +1,24 @@
 <?php
 
-/**
- * getSubsets Function.
- * Clean up the Google Webfonts subsets to be human readable
- *
- * @since ReduxFramework 0.2.0
- */
-function getSubsets( $var ) {
-	$result = array();
-
-	foreach ( $var as $v ) {
-		if ( strpos( $v, "-ext" ) ) {
-			$name = ucfirst( str_replace( "-ext", " Extended", $v ) );
-		} else {
-			$name = ucfirst( $v );
-		}
-
-		array_push(
-			$result, array(
-			'id'   => $v,
-			'name' => $name,
-		)
-		);
-	}
-
-	return array_filter( $result );
-}  //function
-
-/**
- * getVariants Function.
- * Clean up the Google Webfonts variants to be human readable
- *
- * @since ReduxFramework 0.2.0
- */
-function getVariants( $var ) {
-	$result = array();
-	$italic = array();
-
-	foreach ( $var as $v ) {
-		$name = "";
-		if ( $v[0] == 1 ) {
-			$name = 'Thin 100';
-		} elseif ( $v[0] == 2 ) {
-			$name = 'Extra Light 200';
-		} elseif ( $v[0] == 3 ) {
-			$name = 'Light 300';
-		} elseif ( $v[0] == 4 || $v[0] == "r" || $v[0] == "i" ) {
-			$name = 'Regular 400';
-		} elseif ( $v[0] == 5 ) {
-			$name = 'Medium 500';
-		} elseif ( $v[0] == 6 ) {
-			$name = 'Semi-Bold 600';
-		} elseif ( $v[0] == 7 ) {
-			$name = 'Bold 700';
-		} elseif ( $v[0] == 8 ) {
-			$name = 'Extra Bold 800';
-		} elseif ( $v[0] == 9 ) {
-			$name = 'Black 900';
-		}
-
-		if ( $v == "regular" ) {
-			$v = "400";
-		}
-
-		if ( strpos( $v, "italic" ) || $v == "italic" ) {
-			$name .= " Italic";
-			$name = trim( $name );
-			if ( $v == "italic" ) {
-				$v = "400italic";
-			}
-			$italic[] = array(
-				'id'   => $v,
-				'name' => $name,
-			);
-		} else {
-			$result[] = array(
-				'id'   => $v,
-				'name' => $name,
-			);
-		}
-	}
-
-	foreach ( $italic as $item ) {
-		$result[] = $item;
-	}
-
-	return array_filter( $result );
-}   //function
-
 date_default_timezone_set( 'UTC' );
 
 $output = shell_exec( 'git log -1' );
 echo shell_exec( 'git checkout -f master' );
-$gFile             = dirname( __FILE__ ) . '/google_fonts.json';
+
+$count_file  = dirname( __FILE__ ) . '/pages.txt';
+
+require_once dirname( __FILE__ ) . '/vendor/autoload.php';
+
+$key    = $argv[1];
+$token  = new Herbert\Envato\Auth\Token( $key );
+$client = new Herbert\EnvatoClient($token);
+
+$list = $client->catalog->collection(['id' => 4201392 ,'page' => 1]);
+
+file_put_contents( $count_file, $list->results['pagination']['pages'] );
+
+return;
+
 $gFileminPHP       = dirname( __FILE__ ) . '/googlefonts.php';
 
 $fonts             = array();
